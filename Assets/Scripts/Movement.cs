ee1ee7785bb8730beca0007cac75969e7f261efb
larsh <larsh1991@hotmail.com>
2015-09-04 09:52:53 +0200

    Init commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	void Awake () {
+        Invoke("HitBall", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void HitBall()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(20f, 5f), ForceMode2D.Impulse);
+    }
+}
