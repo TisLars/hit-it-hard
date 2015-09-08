9962ae9690b780e303df9404acc70bbc3cb26558
Lars <larsh1991@hotmail.com>
2015-09-08 13:00:45 +0200

    LevelScript with rules & changing bgColor on wall-collision

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    LevelScript level;
+    private Camera cam;
+    Color current;
+    Color bgColor;
+
+    int amountOfHits;
+
+    GameObject LosingScreen;
+
+    void Awake () {
+        level = GameObject.Find("Level").GetComponent<LevelScript>();
+        Invoke("HitBall", 1f);
+        cam = Camera.main;
+        amountOfHits = 0;
+    }
+
+    private void HitBall()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(5f, 20f), ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.name.Contains("Wall"))
+        {
+            if (amountOfHits < level.maximumHitRule)
+            {
+                ChangeBackgroundColor();
+                amountOfHits++;
+            } else
+            {
+                Destroy(gameObject);
+                Debug.Log("You lose! > LevelFailed()");
+                LevelFailed();
+            }
+        }
+    }
+
+    private void LevelFailed()
+    {
+        
+    }
+
+    private void ChangeBackgroundColor()
+    {
+        current = new Color(Random.value, Random.value, Random.value);
+        bgColor = new Color(Random.value, Random.value, Random.value);
+        cam.backgroundColor = Color.Lerp(current, bgColor, 5.0f);
+    }
+}
---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+
+    private Camera cam;
+    Color current;
+    Color bgColor;
+    int amountOfHits;
+
+    void Awake()
+    {
+        cam = Camera.main;
+        amountOfHits = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        ChangeBackgroundColor();
+        amountOfHits++;
+        Debug.Log(amountOfHits);
+    }
+
+    private void ChangeBackgroundColor()
+    {
+        current = new Color(Random.value, Random.value, Random.value);
+        bgColor = new Color(Random.value, Random.value, Random.value);
+        cam.backgroundColor = Color.Lerp(current, bgColor, 5.0f);
+    }
+}
