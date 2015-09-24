a08390f775262337b3a45fcc9b55c8f0ce260b71
Lars <larsh1991@hotmail.com>
2015-09-24 12:46:20 +0200

    Hang to rope implemented

---
+++ Assets/RopeObjectScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeObjectScript : MonoBehaviour {
+
+    GameObject ball;
+    private bool hasPlayer = false;
+    ShootLogicV3 shoot;
+
+	void Update () {
+        //TODO rotate rope between z:-35 z:35
+        transform.Rotate(0, 0, 2 * Time.deltaTime);
+
+        if (hasPlayer)
+        {
+            if (ball.GetComponent<ShootLogicV3>().getIsClicking())
+            {
+                LineRenderer line = ball.GetComponent<LineRenderer>();
+                line.SetPosition(0, ball.transform.position);
+                
+                line.SetPosition(1, shoot.getMouseWorldPoint());
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                hasPlayer = false;
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.name == "Ball")
+        {
+            ball = GameObject.Find(coll.name);
+            Destroy(coll.GetComponent<ShootLogicV3>());
+            coll.gameObject.AddComponent<ShootLogicV3>();
+            shoot = coll.GetComponent<ShootLogicV3>();
+
+            coll.transform.parent = gameObject.transform;
+            coll.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            coll.GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+            hasPlayer = true;
+        }
+    }
+}
