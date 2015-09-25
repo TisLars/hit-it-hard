f1cb65f9ee7a4e7d2e0eaf6a69c7e2d00358ddb0
Lars <larsh1991@hotmail.com>
2015-09-25 11:08:18 +0200

    Implemented tube

--- Assets/Scripts/BasketObjectScript.cs
+++ Assets/Scripts/BasketObjectScript.cs
@@ -18,7 +18,6 @@ public class BasketObjectScript : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D coll)
     {
         moveDirection = Random.Range(-6f, 6f);
-        Debug.Log(moveDirection);
         coll.GetComponent<Rigidbody2D>().AddForce(new Vector2(moveDirection, 25f), ForceMode2D.Impulse);
         StartCoroutine(ShowParticles());
     }
