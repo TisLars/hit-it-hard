a58b93d0eb87171d65dbe7b5f399b5c1b549bf46
Lars <larsh1991@hotmail.com>
2015-10-05 11:47:41 +0200

    Stages 2, 3, 6

--- Assets/Scripts/WaterBasketScript.cs
+++ Assets/Scripts/WaterBasketScript.cs
@@ -5,8 +5,6 @@ public class WaterBasketScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll)
     {
-        Debug.Log(coll.GetComponent<Rigidbody2D>().velocity.x);
-        Debug.Log(coll.GetComponent<Rigidbody2D>().velocity.y);
         coll.GetComponent<Rigidbody2D>().velocity = new Vector2(coll.GetComponent<Rigidbody2D>().velocity.x, 0f);
     }
 }
