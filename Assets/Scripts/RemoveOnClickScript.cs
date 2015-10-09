e66db6a7bffbe844df239e1b561cbb4e288e01b3
Axel Schelfhout <axelschelfhout@gmail.com>
2015-10-09 12:34:14 +0200

    Add new pausemenu & levelpanel & font & lvl1 fix

---
+++ Assets/Scripts/RemoveOnClickScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveOnClickScript : MonoBehaviour {
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/ShootLogicTouchAreaScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootLogicTouchAreaScript : MonoBehaviour {
+
+    private ShootLogicV3 shoot;
+
+	// Use this for initialization
+	void Start () {
+        shoot = transform.parent.GetComponent<ShootLogicV3>();
+    }
+
+    void Update()
+    {
+        if(shoot.isShot)
+        {
+            Debug.Log("HHHH");
+            Destroy(gameObject);
+        }
+    }
+
+}
